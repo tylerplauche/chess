0d705c3b868632731078dd2a411ccac37dc64978
tylerplauche <tylerplauche@gmail.com>
2025-06-28 21:03:52 -0600

    initial(notes) creation

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -7,7 +7,7 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
-
+    //private ChessPiece[][] squares = new ChessPiece[8][8];
     public ChessBoard() {
         
     }
@@ -19,7 +19,7 @@ public class ChessBoard {
      * @param piece    the piece to add
      */
     public void addPiece(ChessPosition position, ChessPiece piece) {
-        throw new RuntimeException("Not implemented");
+        //squares[position.getRow()][position.getColumn()] = piece;
     }
 
     /**
