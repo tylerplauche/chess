631962906c6d3bfd6549f45e5fee1aa5bb04f94c
tylerplauche <tylerplauche@gmail.com>
2025-06-29 18:27:50 -0600

    Queen test works and passed all tests

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -332,6 +332,7 @@ public class ChessPiece {
 
                 while (row >= 1 && row <= 8 && col >= 1 && col <= 8) {
                     ChessPosition newPos = new ChessPosition(row, col);
+                    
                     ChessPiece target = board.getPiece(newPos);
 
                     if (target == null) {
