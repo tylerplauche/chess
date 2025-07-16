3e64a11db0b8b838fe7005b8938d6fa0918b2210
tylerplauche <tylerplauche@gmail.com>
2025-07-16 12:47:56 -0600

    implementing Data Memoru class

---
+++ server/src/main/java/dataaccess/DataMemory.java
@@ -0,0 +1,65 @@
+package dataaccess;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.*;
+
+public class DataMemory implements DataAccess {
+    private final Map<String, UserData> users = new HashMap<>();
+    private final Map<String, AuthData> auths = new HashMap<>();
+    private final Map<Integer, GameData> games = new HashMap<>();
+    private int nextGameID = 1;
+
+    public void clear() {
+        users.clear();
+        auths.clear();
+        games.clear();
+        nextGameID = 1;
+    }
+
+    public void insertUser(UserData user) throws DataAccessException {
+        if (users.containsKey(user.username())) {
+            throw new DataAccessException("User already exists");
+        }
+        users.put(user.username(), user);
+    }
+
+    public UserData getUser(String username) throws DataAccessException {
+        return users.get(username);
+    }
+
+    public void insertAuth(AuthData auth) throws DataAccessException {
+        auths.put(auth.authToken(), auth);
+    }
+
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return auths.get(authToken);
+    }
+
+    public void deleteAuth(String authToken) throws DataAccessException {
+        auths.remove(authToken);
+    }
+
+    public int insertGame(GameData game) throws DataAccessException {
+        int id = nextGameID;
+        nextGameID++;
+        GameData newGame = new GameData(id, game.whiteUsername(), game.blackUsername(), game.gameName(), game.game());
+        games.put(id, newGame);
+        return id;
+    }
+
+    public GameData getGame(int gameID) throws DataAccessException {
+        return games.get(gameID);
+    }
+
+    public Collection<GameData> listGames() throws DataAccessException {
+        return games.values();
+    }
+
+    public void updateGame(GameData game) throws DataAccessException {
+        games.put(game.gameID(), game);
+    }
+}
+
