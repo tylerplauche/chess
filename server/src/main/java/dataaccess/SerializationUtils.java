1d7ed5abdc1efde28665e294bb1273a3165cc61d
tylerplauche <tylerplauche@gmail.com>
2025-07-22 17:26:48 -0600

    code quality

--- server/src/main/java/dataaccess/SerializationUtils.java
+++ server/src/main/java/dataaccess/SerializationUtils.java
@@ -4,13 +4,13 @@ import chess.ChessGame;
 import com.google.gson.Gson;
 
 public class SerializationUtils {
-    private static final Gson gson = new Gson();
+    private static final Gson GSON = new Gson();
 
     public static String serializeGame(ChessGame game) {
-        return gson.toJson(game);
+        return GSON.toJson(game);
     }
 
     public static ChessGame deserializeGame(String json) {
-        return gson.fromJson(json, ChessGame.class);
+        return GSON.fromJson(json, ChessGame.class);
     }
 }
